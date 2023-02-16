1aef7fd08654741c6ca795254ffe32505a3f44bc
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-16 09:05:12 -0800

    Added function description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'This prigram prints an output'
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
