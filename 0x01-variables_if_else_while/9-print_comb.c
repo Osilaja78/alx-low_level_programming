e5149dccd82f74431cc7132f3f01ad8ae7e6e2c0
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-17 10:34:52 -0800

    Added space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,19 +10,20 @@
 int main(void)
 {
 	int num1 = 0;
-	int num2;
 
 	while (num1 <= 9)
 	{
-		num2 = 0;
-		while (num2 <= 9)
+		if (num1 == 9)
 		{
 			putchar('0' + num1);
-			putchar('0' + num2);
-			putchar(',');
-			num2++;
+			num1++;
 		}
-		num1++;
+		else
+		{
+			putchar('0' + num1);
+			putchar(',');
+			num1++;
+		}
 	}
 	putchar('\n');
 	return (0);
