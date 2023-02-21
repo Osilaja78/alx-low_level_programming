f975472ac9729d658aab755cacbb332f53c1e88e
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-21 11:51:07 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <ctype.h>
+#include <stdlib.h>
 
 /**
  * _abs - Entry point
@@ -8,8 +8,10 @@
  *
  * Return: Always 0 (success)
  */
-int _abs(int)
+int _abs(int c)
 {
-	_putchar(abs(int));
+	int absolute = abs(c);
+
+	_putchar(absolute);
 	return (0);
 }
