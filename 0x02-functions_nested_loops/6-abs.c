d5b1ecbb4143ec0a66349ff092490cd1bbba81cd
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-21 11:28:35 -0800

    Print abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _abs - Entry point
+ *
+ * Description: 'This program prints the absolute of a number'
+ *
+ * Return: Always 0 (success)
+ */
+int _abs(int)
+{
+	_putchar(abs(int));
+	return (0);
+}
