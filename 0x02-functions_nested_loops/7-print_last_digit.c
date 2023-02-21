79e44877ae30b7b240841b98fbdfa6ca95122626
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-21 12:27:40 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Description: 'This program prints last digit'
+ *
+ * Return: Always 0 (success)
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
