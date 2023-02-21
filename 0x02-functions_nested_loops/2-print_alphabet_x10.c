f145ca5676e364cd85ccb3c1c47af8017dd8c868
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-21 09:28:39 -0800

    Print 10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'This program prints alphabets 10 times'
+ *
+ * Return: Always 0 (success)
+ */
+char print_alphabet_x10(void)
+{
+	char start;
+	int i = 0;
+
+	while (i < 10)
+	{
+		start = 'a';
+
+		while (start <= 'z')
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return(0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 char print_alphabet();
+char print_alphabet_x10();
