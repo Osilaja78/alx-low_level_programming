f05e1df1e90be35af5725d8e36d107c84f159d6d
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-23 12:12:13 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: 'This program prints 0 - 9'
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
