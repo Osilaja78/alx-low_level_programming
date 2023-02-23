941d311b4ec27bcb60c35e288cf2692b20cb6226
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-23 12:31:15 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: 'This program printd 0 - 9 except 2 & 4'
+ *
+ * Return: Always 0 (success)
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+		}
+		else
+		{
+			_putchar(i + '0');
+			i++;
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
