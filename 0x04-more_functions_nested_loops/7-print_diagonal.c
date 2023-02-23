cc2676c82c8232d778c4f79c07c2b93f3918bd1b
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-23 14:15:22 -0800

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: the no. of time to print diagonal line
+ *
+ * Description: 'This program prints\ n times'
+ *
+ * Return: Always 0 (success)
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j;
+
+	while (i < n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			i++;
+		}
+		else
+		{
+			j = 0;
+
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+}
