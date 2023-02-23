dd7564a7977f9189d7ed53ac923abcf365be11e3
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-23 12:02:20 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: first variable
+ * @b: second variable
+ *
+ * Description: 'This program prints the multiple of two numbers'
+ *
+ * Return: @c - the multiple of both numbers
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
