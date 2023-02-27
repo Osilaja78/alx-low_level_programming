e32c47f09f1ab560a7d076a4b3c8c32a55f01f59
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-27 11:10:04 -0800

    print strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - prints lenght of a string
+ * @s: variable to be checked
+ *
+ * Descriptipn: prints length of string
+ *
+ * Return: Always 0
+ */
+int _strlen(int *s)
+{
+	int b = strlen(*s);
+
+	return (b)
+}
