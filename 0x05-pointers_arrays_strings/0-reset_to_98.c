ed9cb622853177f3af6177ef2585f8d387add8e6
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-27 10:54:23 -0800

    change to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @*n: variable to be checked
+ *
+ * Description: resets 98
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
