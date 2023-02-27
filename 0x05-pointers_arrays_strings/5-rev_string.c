33a1c1ef5d1bf54287f141d389f8d99ba677c7de
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-27 12:59:14 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Enry point
+ * @s: variable
+ *
+ * Description: reverse a string
+ *
+ * Return: Alwats 0
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	int len = strlen(s);
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
