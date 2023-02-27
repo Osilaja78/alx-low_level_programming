90a9a550196ecb085d341b3aa30bd481775d29ba
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-27 13:23:53 -0800

    skip all even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - Entry point
+ * @str: string to be printed
+ *
+ * Desciption: Prints some part of a string.
+ *
+ * Return: Always 0 (success)
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
