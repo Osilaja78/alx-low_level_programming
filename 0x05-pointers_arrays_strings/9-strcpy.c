d306fc7469b628b9498edb77531cbbaac00389df
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-28 00:22:00 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - Entry point
+ * @dest: destination
+ * @src: source
+ *
+ * Description: copies string from source to deatination
+ *
+ * Return: Always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (&dest);
+}
