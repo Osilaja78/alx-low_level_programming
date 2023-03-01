a18bf8ebbcbce70b5407463079f161a114956920
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-02-28 23:59:30 -0800

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Entry point.
+ * @src: source
+ * @dest: destination
+ *
+ * Description: Concatenates two strings.
+ *
+ * Return: Alwats 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(src, dest));
+}
