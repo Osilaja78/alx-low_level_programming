939c992e1176b8bc84a524fc8ec115f38e0e1d6e
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-02 03:20:48 -0800

    fixed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * leet - Entru point
+ * leet - Entry point
  * @str: string to be encoded
  *
  * Description: a function that encodes a string into 1337.
@@ -10,52 +10,20 @@
  */
 char *leet(char *str)
 {
-	int i = 0;
-	char *numbers = "01347";
+	char *letters = "aAeEoOtTlL";
+	char *leet_chars = "4433007711";
+	int i, j;
 
-	while (str[i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a')
+		for (j = 0; j < 10; j++)
 		{
-			str[i] = numbers[3];
+			if (str[i] == letters[j])
+			{
+				str[i] = leet_chars[j];
+				break;
+			}
 		}
-		else if (str[i] == 'A')
-		{
-			str[i] = numbers[3];
-		}
-		else if (str[i] == 'e')
-		{
-			str[i] = numbers[2];
-		}
-		else if (str[i] == 'E')
-		{
-			str[i] = numbers[2];
-		}
-		else if (str[i] == 'o')
-		{
-			str[i] = numbers[0];
-		}
-		else if (str[i] == 'O')
-		{
-			str[i] = numbers[0];
-		}
-		else if (str[i] == 't')
-		{
-			str[i] = numbers[4];
-		}
-		else if (str[i] == 'T')
-		{
-			str[i] = numbers[4];
-		}
-		else if (str[i] == 'l')
-		{
-			str[i] = numbers[1];
-		}
-		else if (str[i] == 'L')
-		{
-			str[i] = numbers[1];
-		}
-		i++;
 	}
 	return (str);
 }
