9769c7956ebd24928bb9a63e6b03da6155a6a6d1
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-01 22:45:27 -0800

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpm - Entry point
+ * @s1: string one
+ * @s2: string two
+ *
+ * Description: Compares two strings using strcmp
+ *
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
