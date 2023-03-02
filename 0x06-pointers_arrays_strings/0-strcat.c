d8833e00442c1df4ce89848057dc3ae31fe7fbcf
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-02 03:50:23 -0800

    hello world

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,5 +12,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(src, dest));
+	int dest_len =0;
+	int i = 0;
+	int j =0;
+
+	while (dest[i] != '\0')
+	{
+		dest_len++;
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[dest_len + j] = src[j];
+		j++;
+	}
+
+	dest[dest_len + j] = '\0';
+	return (dest);
 }
