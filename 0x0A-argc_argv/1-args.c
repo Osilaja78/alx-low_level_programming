c1baacc28bcc864886db2379c1cd5ab539c09814
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-10 14:33:41 -0800

    how many args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: prints the number of argument passed.
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
