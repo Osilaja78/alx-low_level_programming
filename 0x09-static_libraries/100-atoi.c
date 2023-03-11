3e604cca5a5ac37546fdad3171b09fdb8ae6857b
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-10 17:30:16 -0800

    lib my

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
-int _atoi(char *s);
+/**
+ * _atoi - Entry point
+ * @s: string to be converted
+ *
+ * Description: Converts a string to an int.
+ *
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	if (*s)
+	{
+		return (0);
+	}
+	return (0);
+}
