2d26fad0ccdf076c38556361eafd633bcb323c5b
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-14 15:47:49 -0700

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+int _strlen1(char *str1);
+int _strlen2(char *str2);
 /**
  * str_concat - Entry point.
  * @s1: string one.
@@ -21,21 +23,14 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-	else if (s2 == NULL)
+
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
 
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-
+	len1 = _strlen1(s1);
+	len2 = _strlen2(s2);
 	len = len1 + len2;
 	str = malloc((len + 1) * sizeof(char));
 
@@ -59,4 +54,42 @@ char *str_concat(char *s1, char *s2)
 		}
 	}
 	return (str);
+}
+
+/**
+ * _strlen1 - checks the length of str1.
+ * @str1: strinv to be checked.
+ *
+ * Description: lenght of @str1.
+ *
+ * Return: int.
+ */
+int _strlen1(char *str1)
+{
+	int len = 0;
+
+	while (str1[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strlen2 - checks the length of str2.
+ * @str2: string to be checked.
+ *
+ * Description: lenght of str2.
+ *
+ * Return: int
+ */
+int _strlen2(char *str2)
+{
+	int len = 0;
+
+	while (str2[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
