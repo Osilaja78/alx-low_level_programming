0d2df5549c403672722d9edeb6b625cfbd4da9f9
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-14 15:16:52 -0700

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,15 @@ char *str_concat(char *s1, char *s2)
 	int len2 = 0;
 	int len, i, j;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[len1] != '\0')
 	{
 		len1++;
