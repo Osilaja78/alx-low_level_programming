afd8fbc87fa1063e5ba20ef9b1da88690caa1360
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-16 15:43:50 -0700

    array of int

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,20 +14,21 @@
 int *array_range(int min, int max)
 {
 	int *ptr, i;
+	int arr_len = max - min + 1;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
 
-	ptr = malloc(sizeof(int) * (max - min + 1));
+	ptr = malloc(sizeof(int) * arr_len);
 
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i <= max; i++)
+	for (i = 0; i <= arr_len; i++)
 	{
 		ptr[i] = min;
 		min++;
