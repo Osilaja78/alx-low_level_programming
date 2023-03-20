2a88e55ffaaf390675062432730d4c72e9db53dc
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-20 14:57:53 -0700

    removed standard lib functions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,18 +15,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int len1, len2, i, j;
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
+		return (NULL);
+
+	len1 = len2 = 0;
+
+	while (name[len1])
+		len1++;
+
+	while (owner[len2])
+		len2++;
+
+	new_dog->name = malloc(sizeof(new_dog->name) * len1);
+	new_dog->owner = malloc(sizeof(new_dog->owner) * len2);
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
 
-	if (new_dog)
-	{
-		name ? new_dog->name = strdup(name) : NULL;
-		new_dog->age = age;
-		owner ? new_dog->owner = strdup(owner) : NULL;
-	}
+	for (i = 0; i <= len1; i++)
+		new_dog->name[i] = name[i];
+
+	new_dog->age = age;
+
+	for (j = 0; j <= len2; j++)
+		new_dog->owner[j] = owner[j];
+
 	return (new_dog);
 }
