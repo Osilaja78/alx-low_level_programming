888db95d57768ac44bbdc2e2ef9536239fa98c7b
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-20 08:15:23 -0700

    fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -25,7 +25,14 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	}
 
-	printf("Age: %.6f\n", d->age);
+	if (d->age == '\0')
+	{
+		printf("Age: nil\n");
+	}
+	else
+	{
+		printf("Age: %.6f\n", d->age);
+	}
 
 	if (d->owner == NULL)
 	{
