3b85b85bd5d9abbaa204cc0dec00cf0fae004f7d
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-20 08:27:35 -0700

    swithched to ternary operator

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,26 +13,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-
-	if (d->name == NULL)
+	if (d)
 	{
-		printf("Name: nil\n");
-	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-	}
-
-	printf("Age: %f\n", d->age);
-
-	if (d->owner == NULL)
-	{
-		printf("Owner: nil");
-	}
-	else
-	{
-		printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
 }
