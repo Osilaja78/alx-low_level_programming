078f3cbf747a76518ee53edb264f6d2d05f0f1db
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-23 14:06:54 -0700

    covered the last base case

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,12 +13,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ptr;
+	char *temp;
 
 	va_start(ptr, n);
 
 	for (i = 1; i <= n; i++)
 	{
-		printf("%s", va_arg(ptr, char*));
+		temp = va_arg(ptr, char*);
+
+		printf("%s", temp == NULL ? "nil" : temp);
 		if (separator != NULL && i != n)
 			printf("%s", separator);
 	}
