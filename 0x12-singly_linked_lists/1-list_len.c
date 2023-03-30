ada64cc2a0628f76b55d65ce697721dea9748170
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-30 04:55:40 -0700

    added print_len function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_len - Entry point.
+ * @h: pointer to list
+ *
+ * Return: number of elements in list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
