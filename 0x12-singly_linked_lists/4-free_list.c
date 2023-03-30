0fbc9f48962121a3c4ab5ad6df85329017b53cf9
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-03-30 08:20:21 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,7 +13,8 @@ void free_list(list_t *head)
 	list_t *current = head;
 	list_t *next;
 
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		next = current->next;
 		free(current->str);
 		free(current);
