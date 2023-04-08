1dea82adee299c15276bad5fb62c3249b3bba978
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-04-08 08:11:35 -0700

    modigied get_bit fucntion

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit = 1;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
 	bit <<= index;
 
 	return ((n & bit) != 0);
