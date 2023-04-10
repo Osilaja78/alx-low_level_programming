7da8d29a8fdf5d8b386a866e17e69ebe88d0b981
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-04-10 03:48:43 -0700

    fixed betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/**
+ * read_textfile - Entry point.
+ * @filename: name of file to read
+ * @letters: no. of letters to read
+ *
+ * Return: no. of bytes actually read.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -11,15 +18,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
 
 	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
