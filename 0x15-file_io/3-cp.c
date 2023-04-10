93ae400e3a0e87a842b047ca86defa2a16b3354c
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-04-10 09:40:45 -0700

    implenemted cp function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int file_action(char *file_from, char *file_to);
+void close_fd(int fd);
 /**
  * main - Entry point.
  * @argc: arg count
@@ -13,18 +15,90 @@
  */
 int main(int argc, char *argv[])
 {
+	int n;
+
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
+
+	n = file_action(argv[1], argv[2]);
+	return (n);
 }
 
+/**
+ * file_action - Main action.
+ * @file_from: file to copy from
+ * @file_to: file to copy to
+ *
+ * Return: int.
+ */
 int file_action(char *file_from, char *file_to)
 {
 	int fd_1, fd_2;
 	char *buffer;
+	ssize_t bytes_read, bytes_written;
 
 	if (file_from == NULL || file_to == NULL)
 		return (-1);
+
+	fd_1 = open(file_from, O_RDONLY);
+	if (fd_1 == -1)
+	{
+		dprintf(2, "Error: can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		close_fd(fd_1);
+		exit(98);
+	}
+
+	bytes_read = read(fd_1, buffer, 1024);
+	if (bytes_read == -1)
+	{
+		close_fd(fd_1);
+		free(buffer);
+		exit(98);
+	}
+
+	buffer[bytes_read] = '\0';
+	close_fd(fd_1);
+
+	fd_2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_2 == -1)
+	{
+		dprintf(2, "Error: can't write to file %s\n", file_to);
+		exit(99);
+	}
+
+	bytes_written = write(fd_2, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		close_fd(fd_2);
+		free(buffer);
+		exit(99);
+	}
+
+	close_fd(fd_2);
+	free(buffer);
+	return (0);
+}
+
+/**
+ * close_fd - close fd with erro.
+ * @fd: file descriptor to close
+ *
+ * Return: nothing.
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
