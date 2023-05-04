7e376b04e6521319120791868c21ff66e0b40493
Osilaja78 <osilajaabdulhameed@gmail.com>
2023-05-04 19:38:50 +0100

    added 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - Entry point.
+ * @n: number to get index of
+ * @index: index to get
+ *
+ * Return: value of the index or -1 if faled.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = 1;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	bit <<= index;
+
+	return ((n & bit) != 0);
+}
