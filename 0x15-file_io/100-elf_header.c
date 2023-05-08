15e0b0ea691facfad37f04310b84a5b320e21d7a
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-05-08 07:21:26 -0700

    modified 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,72 +2,84 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 #include <elf.h>
 
-#define BUFSIZE 1024
+#define BUF_SIZE 128
 
+/**
+ * print_elf_header - prints necessary elf headers.
+ * @ehdr: ehdr
+ *
+ * Return: nothing.
+ */
+void print_elf_header(Elf64_Ehdr *ehdr)
+{
+	int i;
+
+	printf("Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", ehdr->e_ident[i]);
+	printf("\n");
+
+	printf("Class:                             %s\n", \
+		ehdr->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Invalid Class");
+	printf("Data:                              %s\n", \
+		ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? \
+		"2's complement, little endian" : "Invalid data encoding");
+	printf("Version:                           %d (current)\n", \
+		ehdr->e_ident[EI_VERSION]);
+	printf("OS/ABI:                            %d\n", \
+		ehdr->e_ident[EI_OSABI]);
+	printf("ABI Version:                       %d\n", \
+			ehdr->e_ident[EI_ABIVERSION]);
+	printf("Type:                              %d\n", ehdr->e_type);
+	printf("Entry point address:               0x%lx\n", ehdr->e_entry);
+}
+
+/**
+ * main - entry point.
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: int.
+ */
 int main(int argc, char *argv[])
 {
-    int fd;
-    Elf64_Ehdr header;
-    ssize_t n;
-    char buf[BUFSIZE];
+	int fd;
+	Elf64_Ehdr ehdr;
+	ssize_t n;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        return 98;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(98);
+	}
 
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Error: cannot open file '%s'\n", argv[1]);
-        return 98;
-    }
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error: could not open file '%s'\n", argv[1]);
+		exit(98);
+	}
 
-    n = read(fd, &header, sizeof(header));
-    if (n != sizeof(header) || memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
-        fprintf(stderr, "Error: '%s' is not an ELF file\n", argv[1]);
-        return 98;
-    }
+	n = read(fd, &ehdr, sizeof(ehdr));
+	if (n == -1)
+	{
+		fprintf(stderr, "Error: could not read ELF header\n");
+		exit(98);
+	}
 
-    printf("ELF Header:\n");
-    printf("  Magic: ");
-    for (int i = 0; i < EI_NIDENT; i++) {
-        printf("%02x ", header.e_ident[i]);
-    }
-    printf("\n");
+	if (n != sizeof(ehdr) || ehdr.e_ident[EI_MAG0] != ELFMAG0 || \
+		ehdr.e_ident[EI_MAG1] != ELFMAG1 || ehdr.e_ident[EI_MAG2] \
+		!= ELFMAG2 || ehdr.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "Error: file '%s' is not an ELF file\n", argv[1]);
+		exit(98);
+	}
 
-    printf("  Class:                             %s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-    printf("  Data:                              %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-    printf("  Version:                           %d%s\n", header.e_ident[EI_VERSION], header.e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-    printf("  OS/ABI:                            %d\n", header.e_ident[EI_OSABI]);
-    printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
-
-    printf("  Type:                              ");
-    switch (header.e_type) {
-        case ET_NONE:
-            printf("NONE (None)\n");
-            break;
-        case ET_REL:
-            printf("REL (Relocatable file)\n");
-            break;
-        case ET_EXEC:
-            printf("EXEC (Executable file)\n");
-            break;
-        case ET_DYN:
-            printf("DYN (Shared object file)\n");
-            break;
-        case ET_CORE:
-            printf("CORE (Core file)\n");
-            break;
-        default:
-            printf("<unknown: %d>\n", header.e_type);
-            break;
-    }
-
-    printf("  Entry point address:               0x%lx\n", header.e_entry);
+	print_elf_header(&ehdr);
 
 	close(fd);
-    return 0;
+	return (0);
 }
+
