25f5ffd5137afa370bf7791c2bb5316a0065f2f3
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-19 10:26:54 -0700

    code for task 2

---
+++ 0x18-dynamic_libraries/math1.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers.
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: the sum of two numbers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers.
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: the subtraction of two numbers.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers.
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: the multiplication of two numbers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers.
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: the division of two numbers.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mods two numbers.
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: the modulus of two numbers.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
