752ddc2771febc10c9ad0a043a9007654e47640a
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-10-11 04:44:51 -0700

    code for task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array 
+ * using binary search algorithm.
+ * @array: arrat to be searched.
+ * @size: no of elements in array.
+ * @value: value to search for.
+ *
+ * Return: index of found value.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid, left, right;
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (mid = left; mid < right; mid++)
+			printf("%d, ", array[mid]);
+		printf("%d\n", array[mid]);
+
+		mid = left + (right - left) / 2;
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
